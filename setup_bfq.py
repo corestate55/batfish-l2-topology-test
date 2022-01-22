dbad26f7d558728e405d3085d8a9606371349ad4
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2022-01-22 22:58:07 +0900

    Update for latest pybatfish

--- setup_bfq.py
+++ setup_bfq.py
@@ -1,26 +1,26 @@
-from pybatfish.client.commands import *
-from pybatfish.question.question import load_questions
-from pybatfish.question import bfq
+from pybatfish.client.session import Session
+from pybatfish.datamodel import *
+from pybatfish.datamodel.answer import *
 from pybatfish.datamodel.flow import *
-from sys import argv
-from os import path
-from pprint import pprint
+import pandas as pd
 
-# call this script with `pthon -i` and query interactive.
+# call this script with `python -i` and query interactive.
 if __name__ == "__main__":
-    if argv[1] and path.isdir(argv[1]):
-        load_questions()
-        print(path.abspath(argv[1]))
-        bf_init_snapshot(argv[1], name='sample', overwrite=True)
-    else:
-        print('Usage: python -i %s ./path/to/sample' % argv[0])
+    pd.set_option("display.width", 300)
+    pd.set_option("display.max_columns", 20)
+    pd.set_option("display.max_rows", 200)
+
+    bf = Session(host='localhost')
+    bf.set_network('batfish-test-topology')
+
+# bf.init_snapshot(path, name='snapshot-name', overwrite=True)
 
 # # layer3
-# ans = bfq.edges(edgeType='layer3')
+# ans = bf.q.edges(edgeType='layer3')
 # ans.answer().frame()
 
 # # layer1
-# ans = bfq.edges(edgeType='layer1')
+# ans = bf.q.edges(edgeType='layer1')
 # ans.answer().frame()
 
 # # select edges by host name
